b6d67cc456b75c0cd3d5503f480ccf03f6058851
ashraful-commits <ashrfulalam10000@gmail.com>
2023-09-18 17:12:32 +0600

    digital nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,6 +31,7 @@ const CircularNav = () => {
 
   const buttonRef = useRef(null);
   const menuContainerRef = useRef(null);
+  const menuContainerRef2 = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -38,7 +39,9 @@ const CircularNav = () => {
         buttonRef.current &&
         !buttonRef.current.contains(event.target) &&
         menuContainerRef.current &&
-        !menuContainerRef.current.contains(event.target)
+        menuContainerRef2.current &&
+        !menuContainerRef.current.contains(event.target) &&
+        !menuContainerRef2.current.contains(event.target)
       ) {
         setIsOpen(false);
       }
@@ -56,12 +59,12 @@ const CircularNav = () => {
   }, [isOpen]);
 
   return (
-    <div className="fixed bottom-2 right-2">
+    <div className="fixed top-[55%] right-3">
       <motion.button
         ref={buttonRef}
         initial={false}
         animate={{ rotate: isOpen ? 45 : 0 }}
-        className={`bg-accent hover:bg-secondary text-white font-semibold rounded-full w-14 h-14 flex items-center justify-center transition-transform ${
+        className={`bg-accent group hover:bg-secondary text-white font-semibold rounded-full w-12 h-12 flex items-center justify-center transition-transform ${
           isOpen ? "rotate-45" : ""
         }`}
         onClick={toggleMenu}
@@ -83,10 +86,64 @@ const CircularNav = () => {
         <span
           className={`absolute top-0 ${
             isOpen ? "block" : "hidden"
-          } left-0 w-full h-full z-[-1] bg-secondary hover:bg-primary rounded-full rounded-tl-none`}
+          } left-0 w-full h-full shadow-md z-[-1] bg-accent group-hover:bg-secondary rounded-none`}
         ></span>
       </motion.button>
 
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            ref={menuContainerRef2}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            className="mt-1 z-[-1] space-y-1 h-[50vh] flex justify-between  flex-col bg-background-light bottom-16 right-0 absolute p-1 rounded-md shadow-lg"
+          >
+            <ShopItem label="Home" icon={FaHome} iconColor="text-red-500" />
+            <ShopItem
+              label="Shop"
+              icon={FaShoppingCart}
+              iconColor="text-yellow-500"
+            />
+            <ShopItem
+              label="Product Listings"
+              icon={FaTags}
+              iconColor="text-green-500"
+            />
+            <ShopItem
+              label="Product Detail"
+              icon={FaThumbsUp}
+              iconColor="text-blue-500"
+            />
+
+            <ShopItem
+              label="Search Results"
+              icon={FaSearch}
+              iconColor="text-cyan-500"
+            />
+            <ShopItem
+              label="Contact Us"
+              icon={FaEnvelope}
+              iconColor="text-gray-500"
+            />
+            <ShopItem
+              label="About Us"
+              icon={FaInfoCircle}
+              iconColor="text-red-600"
+            />
+            <ShopItem
+              label="Blog"
+              icon={FaNewspaper}
+              iconColor="text-yellow-600"
+            />
+            <ShopItem
+              label="FAQs"
+              icon={FaQuestionCircle}
+              iconColor="text-green-600"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <AnimatePresence initial={false}>
         {isOpen && (
           <motion.div
@@ -94,73 +151,32 @@ const CircularNav = () => {
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
-            className="mt-1 z-[-1] space-y-1 h-[85vh] flex justify-between  flex-col bg-background-light bottom-16 right-1 absolute p-1 rounded-md shadow-lg"
+            className="mt-1 z-[-1] space-y-1 h-[35vh] flex justify-between  flex-col bg-background-light top-16 right-0 absolute p-1 rounded-md shadow-lg"
           >
-            <NavItem label="Home" icon={FaHome} iconColor="text-red-500" />
-            <NavItem
-              label="Shop"
-              icon={FaShoppingCart}
-              iconColor="text-yellow-500"
-            />
-            <NavItem
-              label="Product Listings"
-              icon={FaTags}
-              iconColor="text-green-500"
-            />
-            <NavItem
-              label="Product Detail"
-              icon={FaThumbsUp}
-              iconColor="text-blue-500"
-            />
-            <NavItem
+            <UserItem
               label="Shopping Cart"
               icon={FaShoppingCart}
               iconColor="text-purple-500"
             />
-            <NavItem
+            <UserItem
               label="Checkout"
               icon={FaCreditCard}
               iconColor="text-pink-500"
             />
-            <NavItem
+            <UserItem
               label="Order Confirmation"
               icon={FaThumbsUp}
               iconColor="text-indigo-500"
             />
-            <NavItem
+            <UserItem
               label="User Account"
               icon={FaUserCircle}
               iconColor="text-teal-500"
             />
-            <NavItem
+            <UserItem
               label="Wishlist"
               icon={FaHeart}
               iconColor="text-orange-500"
-            />
-            <NavItem
-              label="Search Results"
-              icon={FaSearch}
-              iconColor="text-cyan-500"
-            />
-            <NavItem
-              label="Contact Us"
-              icon={FaEnvelope}
-              iconColor="text-gray-500"
-            />
-            <NavItem
-              label="About Us"
-              icon={FaInfoCircle}
-              iconColor="text-red-600"
-            />
-            <NavItem
-              label="Blog"
-              icon={FaNewspaper}
-              iconColor="text-yellow-600"
-            />
-            <NavItem
-              label="FAQs"
-              icon={FaQuestionCircle}
-              iconColor="text-green-600"
             />
           </motion.div>
         )}
@@ -169,7 +185,30 @@ const CircularNav = () => {
   );
 };
 
-const NavItem = ({ label, icon, iconColor }) => {
+const ShopItem = ({ label, icon, iconColor }) => {
+  const Icon = icon;
+
+  return (
+    <div className="relative group transition-transform transform translate-y-0">
+      <a
+        href="#"
+        className={`group relative shadow-sm px-2 py-2 rounded-rounded-bl-none rounded-tl-none hover:bg-white hover:${iconColor} border-l-2 border-white group-hover:border-l-orange-600 transition-transform flex items-center space-x-1 ${iconColor}`}
+      >
+        <motion.i
+          className={`text-2xl  transition-transform transform scale-100`}
+        >
+          <Icon />
+        </motion.i>
+        <span
+          className={`text-sm group-hover:animate-bounce group-hover:border-r-2 border-r-orange-600 absolute whitespace-nowrap shadow-lg opacity-0 group-hover:opacity-100 right-10 text text-center transition-opacity bg-white ${iconColor} rounded-lg rounded-br-none rounded-tr-none py-[.63rem] px-2`}
+        >
+          {label}
+        </span>
+      </a>
+    </div>
+  );
+};
+const UserItem = ({ label, icon, iconColor }) => {
   const Icon = icon;
 
   return (
