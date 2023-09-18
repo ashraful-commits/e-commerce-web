2732823d5adde968228b1110ccb33470feb26f56
ashraful-commits <ashrfulalam10000@gmail.com>
2023-09-18 16:43:14 +0600

    Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import {
   FaShoppingCart,
   FaUser,
@@ -28,11 +29,9 @@ const CircularNav = () => {
     setIsOpen(!isOpen);
   };
 
-  // Use a ref to track the button and menu container
   const buttonRef = useRef(null);
   const menuContainerRef = useRef(null);
 
-  // Add a click event listener to the document
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (
@@ -41,19 +40,16 @@ const CircularNav = () => {
         menuContainerRef.current &&
         !menuContainerRef.current.contains(event.target)
       ) {
-        // Click occurred outside of the button and menu, so close the menu
         setIsOpen(false);
       }
     };
 
-    // Attach the event listener when the menu is open
     if (isOpen) {
       document.addEventListener("click", handleClickOutside);
     } else {
       document.removeEventListener("click", handleClickOutside);
     }
 
-    // Clean up the event listener when the component unmounts
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
@@ -61,9 +57,11 @@ const CircularNav = () => {
 
   return (
     <div className="fixed bottom-2 right-2">
-      <button
+      <motion.button
         ref={buttonRef}
-        className={`bg-primary  hover:bg-secondary text-white font-semibold rounded-full w-14 h-14 flex items-center justify-center transition-transform transform ${
+        initial={false}
+        animate={{ rotate: isOpen ? 45 : 0 }}
+        className={`bg-accent hover:bg-secondary text-white font-semibold rounded-full w-14 h-14 flex items-center justify-center transition-transform ${
           isOpen ? "rotate-45" : ""
         }`}
         onClick={toggleMenu}
@@ -85,81 +83,108 @@ const CircularNav = () => {
         <span
           className={`absolute top-0 ${
             isOpen ? "block" : "hidden"
-          } left-0 w-10 h-10 z-[-1] hover:bg-secondary bg-primary rounded-full rounded-tl-none`}
+          } left-0 w-full h-full z-[-1] bg-secondary hover:bg-primary rounded-full rounded-tl-none`}
         ></span>
-      </button>
+      </motion.button>
 
-      {isOpen && (
-        <div
-          ref={menuContainerRef}
-          className="mt-1 z-[-1] space-y-1 h-[85vh] flex justify-between flex-col bg-background-light bottom-16 right-1 absolute p-1 rounded-md shadow-lg"
-        >
-          <NavItem label="Home" icon={FaHome} color="#FF5733" />
-          <NavItem label="Shop" icon={FaShoppingCart} color="#FFC300" />
-          <NavItem label="Product Listings" icon={FaTags} color="#FF5733" />
-          <NavItem label="Product Detail" icon={FaThumbsUp} color="#FFC300" />
-          <NavItem
-            label="Shopping Cart"
-            icon={FaShoppingCart}
-            color="#FF5733"
-          />
-          <NavItem label="Checkout" icon={FaCreditCard} color="#FFC300" />
-          <NavItem
-            label="Order Confirmation"
-            icon={FaThumbsUp}
-            color="#FF5733"
-          />
-
-          <NavItem label="User Account" icon={FaUserCircle} color="#FFC300" />
-          <NavItem label="Wishlist" icon={FaHeart} color="#FF5733" />
-          <NavItem label="Search Results" icon={FaSearch} color="#FFC300" />
-          <NavItem label="Contact Us" icon={FaEnvelope} color="#FF5733" />
-          <NavItem label="About Us" icon={FaInfoCircle} color="#FFC300" />
-          <NavItem label="Blog" icon={FaNewspaper} color="#FF5733" />
-          <NavItem label="FAQs" icon={FaQuestionCircle} color="#FF5733" />
-          {/* <NavItem label="Returns and Refunds" icon={FaSyncAlt} color="#FFC300" />,
-    <NavItem label="Shipping Information" icon={FaTruck} color="#FF5733" />,
-    <NavItem label="Payment Methods" icon={FaCreditCard} color="#FFC300" />,
-    <NavItem label="Reviews and Ratings" icon={FaThumbsUp} color="#FF5733" />,
-    <NavItem label="User Dashboard" icon={FaUser} color="#FFC300" />,
-    <NavItem label="Admin Dashboard" icon={FaCog} color="#FF5733" />,
-    <NavItem label="Category Pages" icon={FaTags} color="#FFC300" />,
-    <NavItem label="Promotions and Deals" icon={FaThumbsUp} color="#FF5733" />,
-    <NavItem label="Customer Support" icon={FaHeadset} color="#FFC300" />,
-    <NavItem label="Sitemap" icon={FaChartBar} color="#FF5733" />, */}
-        </div>
-      )}
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            ref={menuContainerRef}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            className="mt-1 z-[-1] space-y-1 h-[85vh] flex justify-between  flex-col bg-background-light bottom-16 right-1 absolute p-1 rounded-md shadow-lg"
+          >
+            <NavItem label="Home" icon={FaHome} iconColor="text-red-500" />
+            <NavItem
+              label="Shop"
+              icon={FaShoppingCart}
+              iconColor="text-yellow-500"
+            />
+            <NavItem
+              label="Product Listings"
+              icon={FaTags}
+              iconColor="text-green-500"
+            />
+            <NavItem
+              label="Product Detail"
+              icon={FaThumbsUp}
+              iconColor="text-blue-500"
+            />
+            <NavItem
+              label="Shopping Cart"
+              icon={FaShoppingCart}
+              iconColor="text-purple-500"
+            />
+            <NavItem
+              label="Checkout"
+              icon={FaCreditCard}
+              iconColor="text-pink-500"
+            />
+            <NavItem
+              label="Order Confirmation"
+              icon={FaThumbsUp}
+              iconColor="text-indigo-500"
+            />
+            <NavItem
+              label="User Account"
+              icon={FaUserCircle}
+              iconColor="text-teal-500"
+            />
+            <NavItem
+              label="Wishlist"
+              icon={FaHeart}
+              iconColor="text-orange-500"
+            />
+            <NavItem
+              label="Search Results"
+              icon={FaSearch}
+              iconColor="text-cyan-500"
+            />
+            <NavItem
+              label="Contact Us"
+              icon={FaEnvelope}
+              iconColor="text-gray-500"
+            />
+            <NavItem
+              label="About Us"
+              icon={FaInfoCircle}
+              iconColor="text-red-600"
+            />
+            <NavItem
+              label="Blog"
+              icon={FaNewspaper}
+              iconColor="text-yellow-600"
+            />
+            <NavItem
+              label="FAQs"
+              icon={FaQuestionCircle}
+              iconColor="text-green-600"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 
-const NavItem = ({ label, icon, color }) => {
+const NavItem = ({ label, icon, iconColor }) => {
   const Icon = icon;
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
-      className="relative group transition-transform transform translate-y-0"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+    <div className="relative group transition-transform transform translate-y-0">
       <a
         href="#"
-        className="relative px-2 py-2 rounded-rounded-bl-none rounded-tl-none hover:bg-accent hover:text-text-dark transition-transform flex items-center space-x-1"
+        className={`group relative shadow-sm px-2 py-2 rounded-rounded-bl-none rounded-tl-none hover:bg-white hover:${iconColor} border-l-2 border-white group-hover:border-l-orange-600 transition-transform flex items-center space-x-1 ${iconColor}`}
       >
-        {icon && (
-          <Icon
-            className={`text-2xl ${
-              isHovered ? "text-white" : `bg-${color}`
-            } transition-transform transform ${
-              isHovered ? "scale-110" : "scale-100"
-            }`}
-          />
-        )}
+        <motion.i
+          className={`text-2xl  transition-transform transform scale-100`}
+        >
+          <Icon />
+        </motion.i>
         <span
-          className={`text-sm  absolute whitespace-nowrap  right-10 text text-center opacity-0 ${
-            isHovered ? "opacity-100" : ""
-          } transition-opacity bg-accent text-white rounded-lg rounded-br-none rounded-tr-none py-[.63rem] px-2`}
+          className={`text-sm group-hover:animate-bounce group-hover:border-r-2 border-r-orange-600 absolute whitespace-nowrap shadow-lg opacity-0 group-hover:opacity-100 right-10 text text-center transition-opacity bg-white ${iconColor} rounded-lg rounded-br-none rounded-tr-none py-[.63rem] px-2`}
         >
           {label}
         </span>
