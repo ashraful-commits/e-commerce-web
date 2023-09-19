8175aeec622fd56a9a378edc5397510b47791d26
ashraful-commits <ashrfulalam10000@gmail.com>
2023-09-19 16:21:14 +0600

    shop

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,16 +5,16 @@ import { AiOutlineClose } from "react-icons/ai";
 // Modal component
 const Modal = ({ isOpen, onClose, children }) => {
   return (
-    <div className="fixed top-0  p-5 left-0 z-[999999999] w-screen h-screen flex items-center justify-center ">
+    <div className="fixed top-0  left-0 z-[999999999] w-screen h-screen flex items-center justify-center ">
       <div className="w-full fixed z-0 bg-blend-overlay bg-purple-900 bg-opacity-90 h-full"></div>
-      <div className="modal bg-white z-10 p-4 w-full h-full  rounded-lg shadow-lg">
+      <div className="modal bg-white bg-opacity-50 z-10  w-full h-full  rounded-lg shadow-lg">
         <button
-          className="modal-close absolute top-5 right-10 text-white hover:text-white"
+          className="modal-close absolute top-5 right-10 text-purple-600 hover:text-purple-900"
           onClick={onClose}
         >
           <AiOutlineClose className="text-3xl" />
         </button>
-        <div className="bg-white">{children}</div>
+        <div className="bg-white w-full h-full p-5">{children}</div>
       </div>
     </div>
   );
